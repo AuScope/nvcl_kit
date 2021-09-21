96dca753b4f15f46ed3051f3f3cd6b0b1d7f7ec8
Vincent Fazio <vincent.fazio@csiro.au>
2021-09-21 04:49:39 +0000

    Update install_nvcl_kit.sh

--- notebooks/install_nvcl_kit.sh
+++ notebooks/install_nvcl_kit.sh
@@ -1,4 +1,6 @@
 !python -m venv ~/venvs/myenv && \
 source ~/venvs/myenv/bin/activate && \
 pip install --upgrade pip nvcl_kit && \
-realpath /env/lib/python3.8/site-packages > ~/venvs/myenv/lib/python3.8/site-packages/base_venv.pth
+realpath /env/lib/python3.8/site-packages > ~/venvs/myenv/lib/python3.8/site-packages/base_venv.pth && \
+python -m ipykernel install --user --name=myenv --display-name "NVCL KIT"
+
