eab43aba9e19e2c8cb18f472daa301f4ebbb8a57
Vincent Fazio <vincent.fazio@csiro.au>
2022-03-25 14:32:11 +1100

    Upgrade NT service to https

--- nvcl_kit/param_builder.py
+++ nvcl_kit/param_builder.py
@@ -89,8 +89,8 @@ def param_builder(provider, **options):
 
     # Northern Territory
     elif provider.lower() in ['nt', 'northern territory']:
-        param_obj.WFS_URL = "http://geology.data.nt.gov.au/geoserver/ows"
-        param_obj.NVCL_URL = "http://geology.data.nt.gov.au/NVCLDataServices/"
+        param_obj.WFS_URL = "https://geology.data.nt.gov.au/geoserver/ows"
+        param_obj.NVCL_URL = "https://geology.data.nt.gov.au/NVCLDataServices/"
         param_obj.USE_LOCAL_FILTERING = True
         param_obj.WFS_VERSION = "2.0.0"
 
