f94db4dd131083701d0adaab6b3b647243e19d41
Vincent Fazio <vincent.fazio@csiro.au>
2022-08-30 12:25:12 +1000

    Add packages to notebook kernel install script

--- notebooks/install_nvcl_kit.sh
+++ notebooks/install_nvcl_kit.sh
@@ -1,6 +1,6 @@
 !python -m venv ~/venvs/myenv && \
 source ~/venvs/myenv/bin/activate && \
-pip install --upgrade pip nvcl_kit && \
+pip install --upgrade pip nvcl_kit matplotlib xmltodict pandas && \
 realpath /env/lib/python3.8/site-packages > ~/venvs/myenv/lib/python3.8/site-packages/base_venv.pth && \
 python -m ipykernel install --user --name=myenv --display-name "NVCL KIT"
 
